ec7824db7f79d81a146987b7ebd1318a68550c4e
azotova <azotova@gmail.com>
2014-11-24 23:42:05 -0800

    13 tests passed

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,16 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-});
+  $scope.data = {links: []};
+
+  angular.extend($scope, Links);
+
+  $scope.getLinks()
+    .then(function(data) {
+      $scope.data.links = data;
+    })
+    .catch(function(er) {console.error(er);});
+
+
+})
+
